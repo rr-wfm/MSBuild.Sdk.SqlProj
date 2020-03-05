e77754d975e1097a615af4a4d28a9a0585a3fd42
Jonathan Mezach <Jonathanm@infosupport.com>
2020-03-05 15:35:21 +0100

    Initial code

---
+++ src/BuildDacpac/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.SqlServer.Dac.Model;
+
+namespace MSBuild.Sdk.SqlProj.BuildDacpac
+{
+    public static class Extensions
+    {
+        public static void AddReference(this TSqlModel model, string referencePath)
+        {
+            var service = model.GetType().GetField("_service", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(model);
+            var dataSchemaModel = service.GetType().GetProperty("DataSchemaModel", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(service);
+
+            var customData = Activator.CreateInstance(Type.GetType("Microsoft.Data.Tools.Schema.SchemaModel.CustomSchemaData, Microsoft.Data.Tools.Schema.Sql"), "Reference", "SqlSchema");
+            var setMetadataMethod = customData.GetType().GetMethod("SetMetadata", BindingFlags.Public | BindingFlags.Instance);
+            setMetadataMethod.Invoke(customData, new object[] { "FileName", referencePath });
+            setMetadataMethod.Invoke(customData, new object[] { "LogicalName", Path.GetFileName(referencePath) });
+            setMetadataMethod.Invoke(customData, new object[] { "SuppressMissingDependenciesErrors", "False" });
+
+            var addCustomDataMethod = dataSchemaModel.GetType().GetMethod("AddCustomData", BindingFlags.Public | BindingFlags.Instance);
+            addCustomDataMethod.Invoke(dataSchemaModel, new object[] { customData });
+        }
+    }
+}
---
+++ src/BuildDacpac/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.SqlServer.Dac;
+using Microsoft.SqlServer.Dac.Model;
+
+namespace MSBuild.Sdk.SqlProj.BuildDacpac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tSqlModelOptions = new TSqlModelOptions
+            {
+                AnsiNullsOn = true,
+                Collation = "SQL_Latin1_General_CP1_CI_AI",
+                CompatibilityLevel = 110,
+                QuotedIdentifierOn = true,
+            };
+
+            var outputFilename = "output.dacpac";
+            var outputFilenameArgument = args.FirstOrDefault(a => a.StartsWith("-o:"));
+            if (outputFilenameArgument != null)
+            {
+                outputFilename = outputFilenameArgument.Substring(3);
+            }
+
+            using (var tSqlModel = new TSqlModel(SqlServerVersion.Sql110, tSqlModelOptions))
+            {
+                foreach (var item in args)
+                {
+                    if (item.StartsWith("-r:"))
+                    {
+                        var reference = item.Substring(3);
+                        System.Console.WriteLine($"Adding reference to {reference}...");
+                        tSqlModel.AddReference(reference);
+                    }
+                    else if (!item.StartsWith("-o:"))
+                    {
+                        System.Console.WriteLine($"Adding {item} to model...");
+                        tSqlModel.AddObjects(File.ReadAllText(item));
+                    }
+                }
+
+                if (File.Exists(outputFilename))
+                {
+                    File.Delete(outputFilename);
+                }
+
+                System.Console.WriteLine($"Writing model to {outputFilename}");
+                DacPackageExtensions.BuildPackage(outputFilename, tSqlModel, new PackageMetadata { Name = outputFilename }, new PackageOptions { });
+            }
+        }
+    }
+}
