96a84a8d0067fd7e7bd1c4895f4b35829f9862d8
Jonathan Mezach <jonathanm@infosupport.com>
2020-03-09 16:01:01 +0100

    Fix a couple of issues

--- src/BuildDacpac/Program.cs
+++ src/BuildDacpac/Program.cs
@@ -17,7 +17,7 @@ namespace MSBuild.Sdk.SqlProj.BuildDacpac
                 new Option<string>(new string[] { "--name", "-n" }, "Name of the package"),
                 new Option<string>(new string[] { "--version", "-v" }, "Version of the package"),
                 new Option<FileInfo>(new string[] { "--output", "-o" }, "Filename of the output package"),
-                new Option<SqlServerVersion>(new string[] { "--sqlServerVersion", "-sv" }, "Target version of the model"), 
+                new Option<SqlServerVersion>(new string[] { "--sqlServerVersion", "-sv" }, () => SqlServerVersion.Sql150, description: "Target version of the model"), 
                 new Option<FileInfo[]>(new string[] { "--input", "-i" }, "Input file name(s)"),
                 new Option<FileInfo[]>(new string[] { "--reference", "-r" }, "Reference(s) to include"),
                 new Option<string[]>(new string[] { "--property", "-p" }, "Properties to be set on the model"),
@@ -40,10 +40,13 @@ namespace MSBuild.Sdk.SqlProj.BuildDacpac
             using var packageBuilder = new PackageBuilder();
             packageBuilder.SetMetadata(name, version);
 
-            foreach (var propertyValue in property)
+            if (property != null)
             {
-                string[] keyValuePair = propertyValue.Split('=', 2);
-                packageBuilder.SetProperty(keyValuePair[0], keyValuePair[1]);
+                foreach (var propertyValue in property)
+                {
+                    string[] keyValuePair = propertyValue.Split('=', 2);
+                    packageBuilder.SetProperty(keyValuePair[0], keyValuePair[1]);
+                }
             }
 
             packageBuilder.UsingVersion(sqlServerVersion);
