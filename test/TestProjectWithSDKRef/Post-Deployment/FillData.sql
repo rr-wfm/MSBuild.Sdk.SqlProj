1073a65473a173e368f3018f79c95d3843680904
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2020-08-05 14:18:15 -0500

    Improve cross-platform handling for PackageReference
    A couple of issues were fixed with respect to how PackageReference's were resolved on non-Windows platforms. This should work as expected now. Thanks to @jeffrosenberg for his contribution.
    
    Resolves #28
    
    Co-authored-by: Rosenberg, Jeff <jeff.rosenberg@icfnext.com>
    Co-authored-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>

--- test/TestProjectWithSDKRef/Post-Deployment/FillData.sql
+++ test/TestProjectWithSDKRef/Post-Deployment/FillData.sql
@@ -1,3 +1,7 @@
 INSERT INTO [dbo].[TestTable] ([Column1])
 VALUES ('FirstRow'),
-       ('SecondRow')
+       ('SecondRow');
+
+INSERT INTO [dbo].[MyTable] ([Column1],[Column2])
+VALUES ('FirstRow', 1),
+       ('SecondRow', 2);
