9fed2b90898e4004ba3ab85f243440bb1109dcbe
Erik Ejlskov Jensen <eej@delegate.dk>
2022-11-23 10:23:54 +0100

    Update System.CommandLine

--- src/DacpacTool/Program.cs
+++ src/DacpacTool/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.CommandLine;
-using System.CommandLine.Invocation;
+using System.CommandLine.NamingConventionBinder;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
