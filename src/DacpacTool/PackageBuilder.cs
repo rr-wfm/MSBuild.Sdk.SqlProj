8ee7ac1a90bb8b7aadc68f6761cf494114d0a049
Erik Ejlskov Jensen <eej@delegate.dk>
2024-03-07 14:04:27 +0100

    PR updates

--- src/DacpacTool/PackageBuilder.cs
+++ src/DacpacTool/PackageBuilder.cs
@@ -69,7 +69,7 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
                 throw new ArgumentException($"Unable to find input file {inputFile}", nameof(inputFile));
             }
 
-            // Skip custom rules files
+            // Skip custom rules files, they will be added to the tools folder later by the analyzer
             if (inputFile.Directory.Name.Equals("rules", StringComparison.OrdinalIgnoreCase)
                 && inputFile.Extension.EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
             {
