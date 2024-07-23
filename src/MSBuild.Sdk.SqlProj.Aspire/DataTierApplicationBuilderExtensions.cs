671e377a44aac301a0888c629ac5a1c424e8f7a3
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2024-07-23 15:52:41 +0200

    Add support for local dacpac
    Signed-off-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>

--- src/MSBuild.Sdk.SqlProj.Aspire/DataTierApplicationBuilderExtensions.cs
+++ src/MSBuild.Sdk.SqlProj.Aspire/DataTierApplicationBuilderExtensions.cs
@@ -7,11 +7,21 @@ namespace Aspire.Hosting;
 
 public static class DataTierApplicationBuilderExtensions
 {
+    /// <summary>
+    /// Adds a data-tier application resource to the application based on a referenced project.
+    /// </summary>
+    /// <typeparam name="TProject">Type that represents the project that produces the data-tier application package.</typeparam>
+    /// <param name="builder">An <see cref="IDistributedApplicationBuilder"/> instance to add the data-tier application to.</param>
+    /// <param name="name">Name of the resource.</param>
+    /// <returns>An <see cref="IResourceBuilder{T}"/> that can be used to further customize the resource.</returns>
     public static IResourceBuilder<DataTierApplicationResource> AddDataTierApplication<TProject>(this IDistributedApplicationBuilder builder, string name)
         where TProject : IProjectMetadata, new()
     {
-        MSBuildLocator.RegisterInstance(MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(
-            instance => instance.Version).First());
+        if (!MSBuildLocator.IsRegistered)
+        {
+            MSBuildLocator.RegisterInstance(MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(
+                instance => instance.Version).First());
+        }
 
         var resource = new DataTierApplicationResource(name);
         
@@ -19,11 +29,37 @@ public static class DataTierApplicationBuilderExtensions
                       .WithAnnotation(new TProject());
     }
 
+    /// <summary>
+    /// Adds a data-tier application resource to the application based on a data-tier application package file.
+    /// </summary>
+    /// <param name="builder">An <see cref="IDistributedApplicationBuilder"/> instance to add the data-tier application to.</param>
+    /// <param name="name">Name of the resource.</param>
+    /// <param name="dacpacPath">Path to a data-tier application package file.</param>
+    /// <returns>An <see cref="IResourceBuilder{T}"/> that can be used to further customize the resource.</returns>
+    public static IResourceBuilder<DataTierApplicationResource> AddDataTierApplication(this IDistributedApplicationBuilder builder, string name, string dacpacPath)
+    {
+        var resource = new DataTierApplicationResource(name);
+
+        if (!Path.IsPathRooted(dacpacPath))
+        {
+            dacpacPath = Path.Combine(builder.AppHostDirectory, dacpacPath);
+        }
+        
+        return builder.AddResource(resource)
+                      .WithAnnotation(new DacpacMetadataAnnotation(dacpacPath));
+    }
+
+    /// <summary>
+    /// Publishes the data-tier application to the target <see cref="SqlServerDatabaseResource"/>.
+    /// </summary>
+    /// <param name="builder">An <see cref="IResourceBuilder{T}"/> representing the data-tier application to publish.</param>
+    /// <param name="target">An <see cref="IResourceBuilder{T}"/> representing the target <see cref="SqlServerDatabaseResource"/> to publish the data-tier application to.</param>
+    /// <returns>An <see cref="IResourceBuilder{T}"/> that can be used to further customize the resource.</returns>
     public static IResourceBuilder<DataTierApplicationResource> PublishTo(
-        this IResourceBuilder<DataTierApplicationResource> builder, IResourceBuilder<SqlServerDatabaseResource> project)
+        this IResourceBuilder<DataTierApplicationResource> builder, IResourceBuilder<SqlServerDatabaseResource> target)
     {
         builder.ApplicationBuilder.Services.TryAddLifecycleHook<PublishDataTierApplicationLifecycleHook>();
-        builder.WithAnnotation(new TargetDatabaseResourceAnnotation(project.Resource.Name), ResourceAnnotationMutationBehavior.Replace);
+        builder.WithAnnotation(new TargetDatabaseResourceAnnotation(target.Resource.Name), ResourceAnnotationMutationBehavior.Replace);
         return builder;
     }
 }
--- src/MSBuild.Sdk.SqlProj.Aspire/DataTierApplicationResource.cs
+++ src/MSBuild.Sdk.SqlProj.Aspire/DataTierApplicationResource.cs
@@ -8,9 +8,20 @@ public sealed class DataTierApplicationResource(string name) : Resource(name)
 {
     public string GetDacpacPath()
     {
-        var projectMetadata = Annotations.OfType<IProjectMetadata>().Single();
-        var projectPath = projectMetadata.ProjectPath;
-        var project = new Project(projectPath);
-        return project.GetPropertyValue("TargetPath");
+        var projectMetadata = Annotations.OfType<IProjectMetadata>().FirstOrDefault();
+        if (projectMetadata != null)
+        {
+            var projectPath = projectMetadata.ProjectPath;
+            var project = new Project(projectPath);
+            return project.GetPropertyValue("TargetPath");
+        }
+
+        var dacpacMetadata = Annotations.OfType<DacpacMetadataAnnotation>().FirstOrDefault();
+        if (dacpacMetadata != null)
+        {
+            return dacpacMetadata.DacpacPath;
+        }
+
+        throw new InvalidOperationException($"Unable to locate data-tier application package for resource {Name}.");
     }
 }
