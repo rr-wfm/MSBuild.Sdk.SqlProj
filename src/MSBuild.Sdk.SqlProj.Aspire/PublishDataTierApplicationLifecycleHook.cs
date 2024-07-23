60623d86322a92127f2daa5c229a8d68020234c4
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2024-07-23 15:52:41 +0200

    Add support for local dacpac
    Signed-off-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>

--- src/MSBuild.Sdk.SqlProj.Aspire/PublishDataTierApplicationLifecycleHook.cs
+++ src/MSBuild.Sdk.SqlProj.Aspire/PublishDataTierApplicationLifecycleHook.cs
@@ -1,3 +1,4 @@
+using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Lifecycle;
 using Microsoft.Extensions.Logging;
@@ -10,7 +11,8 @@ public class PublishDataTierApplicationLifecycleHook : IDistributedApplicationLi
     private readonly ResourceLoggerService _resourceLoggerService;
     private readonly ResourceNotificationService _resourceNotificationService;
 
-    public PublishDataTierApplicationLifecycleHook(ResourceLoggerService resourceLoggerService, ResourceNotificationService resourceNotificationService)
+    public PublishDataTierApplicationLifecycleHook(ResourceLoggerService resourceLoggerService,
+        ResourceNotificationService resourceNotificationService, DistributedApplicationOptions options)
     {
         _resourceLoggerService = resourceLoggerService ?? throw new ArgumentNullException(nameof(resourceLoggerService));
         _resourceNotificationService = resourceNotificationService ?? throw new ArgumentNullException(nameof(resourceNotificationService));
@@ -20,11 +22,20 @@ public class PublishDataTierApplicationLifecycleHook : IDistributedApplicationLi
     {
         foreach (var dataTierApplication in application.Resources.OfType<DataTierApplicationResource>())
         {
+            var logger = _resourceLoggerService.GetLogger(dataTierApplication);
+
+            var dacpacPath = dataTierApplication.GetDacpacPath();
+            if (!File.Exists(dacpacPath))
+            {
+                logger.LogError("Data-tier application package not found at path {DacpacPath}.", dacpacPath);
+                await _resourceNotificationService.PublishUpdateAsync(dataTierApplication,
+                    state => state with { State = new ResourceStateSnapshot("Failed", KnownResourceStateStyles.Error) });
+                continue;
+            }
+
             var targetDatabaseResourceName = dataTierApplication.Annotations.OfType<TargetDatabaseResourceAnnotation>().Single().TargetDatabaseResourceName;
             var targetDatabaseResource = application.Resources.OfType<SqlServerDatabaseResource>().Single(r => r.Name == targetDatabaseResourceName);
             var connectionString = await targetDatabaseResource.ConnectionStringExpression.GetValueAsync(cancellationToken);
-
-            var logger = _resourceLoggerService.GetLogger(dataTierApplication);
 
             await _resourceNotificationService.PublishUpdateAsync(dataTierApplication,
                 state => state with { State = new ResourceStateSnapshot("Publishing", KnownResourceStateStyles.Info) });
@@ -34,7 +45,7 @@ public class PublishDataTierApplicationLifecycleHook : IDistributedApplicationLi
                 var dacServices = new DacServices(connectionString);
                 dacServices.Message += (sender, args) => logger.LogInformation(args.Message.ToString());
 
-                var dacpacPackage = DacPackage.Load(dataTierApplication.GetDacpacPath(), DacSchemaModelStorageType.Memory);
+                var dacpacPackage = DacPackage.Load(dacpacPath, DacSchemaModelStorageType.Memory);
                 dacServices.Deploy(dacpacPackage, targetDatabaseResource.Name, true, new DacDeployOptions(), cancellationToken);
 
                 await _resourceNotificationService.PublishUpdateAsync(dataTierApplication,
--- test/TestAspireHost/Program.cs
+++ test/TestAspireHost/Program.cs
@@ -3,7 +3,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 var sql = builder.AddSqlServer("sql")
                  .AddDatabase("test");
 
-builder.AddDataTierApplication<Projects.TestProject>("db")
+builder.AddDataTierApplication<Projects.TestProject>("testproject")
+       .PublishTo(sql);
+
+builder.AddDataTierApplication("testprojectwithwarnings", "../TestProjectWithWarnings/bin/Debug/netstandard2.0/TestProjectWithWarnings.dacpac")
        .PublishTo(sql);
 
 builder.Build().Run();
