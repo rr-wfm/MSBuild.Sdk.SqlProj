6375e2744477b86bbb3e072843c1480dc7f7c3c6
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2024-07-24 08:54:09 +0200

    Process comments
    Signed-off-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>

--- src/MSBuild.Sdk.SqlProj.Aspire/DataTierApplicationBuilderExtensions.cs
+++ src/MSBuild.Sdk.SqlProj.Aspire/DataTierApplicationBuilderExtensions.cs
@@ -8,9 +8,9 @@ namespace Aspire.Hosting;
 public static class DataTierApplicationBuilderExtensions
 {
     /// <summary>
-    /// Adds a data-tier application resource to the application based on a referenced project.
+    /// Adds a data-tier application resource to the application based on a referenced MSBuild.Sdk.SqlProj project.
     /// </summary>
-    /// <typeparam name="TProject">Type that represents the project that produces the data-tier application package.</typeparam>
+    /// <typeparam name="TProject">Type that represents the project that produces the data-tier application package (.dacpac file).</typeparam>
     /// <param name="builder">An <see cref="IDistributedApplicationBuilder"/> instance to add the data-tier application to.</param>
     /// <param name="name">Name of the resource.</param>
     /// <returns>An <see cref="IResourceBuilder{T}"/> that can be used to further customize the resource.</returns>
@@ -30,11 +30,11 @@ public static class DataTierApplicationBuilderExtensions
     }
 
     /// <summary>
-    /// Adds a data-tier application resource to the application based on a data-tier application package file.
+    /// Adds a data-tier application resource to the application based on a data-tier application package file (.dacpac).
     /// </summary>
     /// <param name="builder">An <see cref="IDistributedApplicationBuilder"/> instance to add the data-tier application to.</param>
     /// <param name="name">Name of the resource.</param>
-    /// <param name="dacpacPath">Path to a data-tier application package file.</param>
+    /// <param name="dacpacPath">Path to a data-tier application package file (.dacpac).</param>
     /// <returns>An <see cref="IResourceBuilder{T}"/> that can be used to further customize the resource.</returns>
     public static IResourceBuilder<DataTierApplicationResource> AddDataTierApplication(this IDistributedApplicationBuilder builder, string name, string dacpacPath)
     {
