9f27f26c2dcbc2b4ac26d7ac792871269dac9e2b
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2024-10-11 16:11:03 +0200

    Remove unused class
    Signed-off-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>

--- src/MSBuild.Sdk.SqlProj.Aspire/TargetDatabaseResourceAnnotation.cs
+++
@@ -1,7 +0,0 @@
-using Aspire.Hosting.ApplicationModel;
-
-namespace MSBuild.Sdk.SqlProj.Aspire;
-
-public record TargetDatabaseResourceAnnotation(string TargetDatabaseResourceName) : IResourceAnnotation
-{
-}
