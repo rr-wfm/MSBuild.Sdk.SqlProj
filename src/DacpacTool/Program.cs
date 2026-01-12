f88d28854ac1578de36b5b92cfc0f683a38f3995
Erik Ejlskov Jensen <eej@delegate.dk>
2026-01-12 13:24:00 +0100

    Update System.CommandLine

--- src/DacpacTool/Program.cs
+++ src/DacpacTool/Program.cs
@@ -1,91 +1,32 @@
 ﻿using System;
-using System.CommandLine;
-using System.CommandLine.NamingConventionBinder;
-using System.CommandLine.Parsing;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using DotMake.CommandLine;
 using Microsoft.SqlServer.Dac;
 using Microsoft.SqlServer.Dac.Model;
 
 namespace MSBuild.Sdk.SqlProj.DacpacTool
 {
+    [CliCommand(Description = "Command line tool for generating a SQL Server Data-Tier Application Framework package (dacpac)", 
+                Children = new[] { typeof(BuildOptions), typeof(InspectOptions), typeof(DeployOptions) })]
+    sealed class RootCommand
+    {
+    }
+
     sealed class Program
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification = "Not called repeatedly")]
         static async Task<int> Main(string[] args)
         {
-            var buildCommand = new Command("build")
+            return await Cli.RunAsync<RootCommand>(args, new CliSettings
             {
-                new Option<string>(new string[] { "--name", "-n" }, "Name of the package") { IsRequired = true },
-                new Option<string>(new string[] { "--version", "-v" }, "Version of the package"),
-                new Option<FileInfo>(new string[] { "--output", "-o" }, "Filename of the output package"),
-                new Option<SqlServerVersion>(new string[] { "--sqlServerVersion", "-sv" }, () => SqlServerVersion.Sql150, description: "Target version of the model"),
-                new Option<FileInfo>(new string[] { "--inputfile", "-i" }, "Text file listing all input files"),
-                new Option<string[]>(new string[] { "--reference", "-r" }, "Reference(s) to include"),
-                new Option<FileInfo>(new string[] { "--predeploy" }, "Filename of optional pre-deployment script"),
-                new Option<FileInfo>(new string[] { "--postdeploy" }, "Filename of optional post-deployment script"),
-                new Option<FileInfo>(new string[] { "--refactorlog" }, "Filename of optional refactor log script"),
-                new Option<string[]>(new string[] { "--buildproperty", "-bp" }, "Build properties to be set on the model"),
-                new Option<string[]>(new string[] { "--deployproperty", "-dp" }, "Deploy properties to be set for the create script"),
-                new Option<string[]>(new string[] { "--sqlcmdvar", "-sc" }, "SqlCmdVariable(s) to include"),
-                
-                new Option<bool>(new string[] { "--runcodeanalysis", "-an" }, "Run static code analysis"),
-                new Option<string>(new string[] { "--codeanalysisrules", "-ar" }, "List of rules to suppress in format '-Microsoft.Rules.Data.SR0001;-Microsoft.Rules.Data.SR0008'"),
-                new Option<FileInfo[]>(new string[] { "--codeanalysisassemblies", "-aa" }, "Custom code analysis rule assemblies to use"),
-
-                new Option<bool>(new string[] { "--warnaserror" }, "Treat T-SQL Warnings As Errors"),
-                new Option<bool>(new string[] { "--generatecreatescript", "-gcs" }, "Generate create script for package"),
-                new Option<bool>(new string[] { "--includecompositeobjects", "-ico" }, "Include referenced, external elements that also compose the source model"),
-                new Option<string>(new string[] { "--targetdatabasename", "-tdn" }, "Name of the database to use in the generated create script"),
-                new Option<string>(new string[] { "--suppresswarnings", "-spw" }, "Warning(s) to suppress"),
-                new Option<FileInfo>(new string[] { "--suppresswarningslistfile", "-spl" }, "Filename for warning(s) to suppress for particular files"),
-#if DEBUG
-                new Option<bool>(new string[] { "--debug" }, "Waits for a debugger to attach")
-#endif
-            };
-            buildCommand.Handler = CommandHandler.Create<BuildOptions>(async (buildOptions) =>
-            {
-                await BuildDacpac(buildOptions).ConfigureAwait(false);
-            });
-
-            var collectIncludesCommand = new Command("collect-includes")
-            {
-                new Option<FileInfo>(new string[] { "--predeploy" }, "Filename of optional pre-deployment script"),
-                new Option<FileInfo>(new string[] { "--postdeploy" }, "Filename of optional post-deployment script"),
-#if DEBUG
-                new Option<bool>(new string[] { "--debug" }, "Waits for a debugger to attach")
-#endif
-            };
-            collectIncludesCommand.Handler = CommandHandler.Create<InspectOptions>(InspectIncludes);
-
-            var deployCommand = new Command("deploy")
-            {
-                new Option<FileInfo>(new string[] { "--input", "-i" }, "Path to the .dacpac package to deploy"),
-                new Option<string>(new string[] { "--targetServerName", "-tsn" }, "Name of the server to deploy the package to"),
-                new Option<int>(new string[] { "--targetPort", "-tprt" }, "Port number to connect on (leave blank for default)"),
-                new Option<string>(new string[] { "--targetDatabaseName", "-tdn" }, "Name of the database to deploy the package to"),
-                new Option<string>(new string[] { "--targetUser", "-tu" }, "Username used to connect to the target server, using SQL Server authentication"),
-                new Option<string>(new string[] { "--targetPassword", "-tp" }, "Password used to connect to the target server, using SQL Server authentication"),
-                new Option<string[]>(new string[] { "--property", "-p" }, "Properties used to control the deployment"),
-                new Option<string[]>(new string[] { "--sqlcmdvar", "-sc" }, "SqlCmdVariable(s) and their associated values, separated by an equals sign."),
-                new Option<bool>(new string[] { "--runScriptsFromReferences", "-sff" }, "Whether to run pre- and postdeployment scripts from references"),
-                new Option<bool>(new string[] { "--encrypt", "-e" }, "Encrypt the connection, defaults to false"),
-#if DEBUG
-                new Option<bool>(new string[] { "--debug" }, "Waits for a debugger to attach")
-#endif
-            };
-            deployCommand.Handler = CommandHandler.Create<DeployOptions>(DeployDacpac);
-
-            var rootCommand = new RootCommand { buildCommand, collectIncludesCommand, deployCommand };
-            rootCommand.Description = "Command line tool for generating a SQL Server Data-Tier Application Framework package (dacpac)";
-
-            var processed = rootCommand.Parse(args);
-            return await processed.InvokeAsync().ConfigureAwait(false);
+                // Configure settings if needed
+            }).ConfigureAwait(false);
         }
 
-        private static async Task<int> BuildDacpac(BuildOptions options)
+        internal static async Task<int> BuildDacpac(BuildOptions options)
         {
             // Wait for a debugger to attach
             WaitForDebuggerToAttach(options);
@@ -222,7 +163,7 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
             return 0;
         }
 
-        private static int InspectIncludes(InspectOptions options)
+        internal static int InspectIncludes(InspectOptions options)
         {
             var scriptInspector = new ScriptInspector();
 
@@ -246,7 +187,7 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
             return 0;
         }
 
-        private static int DeployDacpac(DeployOptions options)
+        internal static int DeployDacpac(DeployOptions options)
         {
             // Wait for a debugger to attach
             WaitForDebuggerToAttach(options);
@@ -318,8 +259,9 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
         }
 
         [Conditional("DEBUG")]
-        private static void WaitForDebuggerToAttach(BaseOptions options)
+        internal static void WaitForDebuggerToAttach(BaseOptions options)
         {
+#if DEBUG
             if (options.Debug)
             {
                 Console.WriteLine($"Waiting for debugger to attach ({Environment.ProcessId})");
@@ -331,6 +273,7 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
                 Console.WriteLine(@"Debugger attached");
 #pragma warning restore CA1303 // Do not pass literals as localized parameters
             }
+#endif
         }
     }
 }
