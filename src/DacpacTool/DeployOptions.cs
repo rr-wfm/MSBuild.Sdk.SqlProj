ba1c61f6901cc8582707421e7ba05f1befc559ce
Erik Ejlskov Jensen <eej@delegate.dk>
2026-01-12 13:36:53 +0100

    add missing aliases

--- src/DacpacTool/DeployOptions.cs
+++ src/DacpacTool/DeployOptions.cs
@@ -9,22 +9,22 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
     internal sealed class DeployOptions : BaseOptions
 #pragma warning restore CA1812 // Avoid uninstantiated internal classes
     {
-        [CliOption(Description = "Path to the .dacpac package to deploy", Required = true, Name = "--input")]
+        [CliOption(Description = "Path to the .dacpac package to deploy", Required = true, Name = "--input", Alias = "-i")]
         public FileInfo Input { get; set; }
         
-        [CliOption(Description = "Name of the server to deploy the package to", Required = true, Name = "--targetServerName")]
+        [CliOption(Description = "Name of the server to deploy the package to", Required = true, Name = "--targetServerName", Alias = "-tsn")]
         public string TargetServerName { get; set; }
         
-        [CliOption(Description = "Port number to connect on (leave blank for default)", Name = "--targetPort", Required = false)]
+        [CliOption(Description = "Port number to connect on (leave blank for default)", Name = "--targetPort", Required = false, Alias = "-tprt")]
         public int? TargetPort { get; set; }
         
-        [CliOption(Description = "Name of the database to deploy the package to", Required = true, Name = "--targetDatabaseName")]
+        [CliOption(Description = "Name of the database to deploy the package to", Required = true, Name = "--targetDatabaseName", Alias = "-tdn")]
         public string TargetDatabaseName { get; set; }
         
-        [CliOption(Description = "Username used to connect to the target server, using SQL Server authentication", Required = false, Name = "--targetUser")]
+        [CliOption(Description = "Username used to connect to the target server, using SQL Server authentication", Required = false, Name = "--targetUser", Alias = "-tu")]
         public string TargetUser { get; set; }
         
-        [CliOption(Description = "Password used to connect to the target server, using SQL Server authentication", Required = false, Name = "--targetPassword")]
+        [CliOption(Description = "Password used to connect to the target server, using SQL Server authentication", Required = false, Name = "--targetPassword", Alias = "-tp")]
         public string TargetPassword { get; set; }
         
         [CliOption(Description = "Properties used to control the deployment", Required = false, Name = "--property")]
@@ -33,10 +33,10 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
         [CliOption(Description = "SqlCmdVariable(s) and their associated values, separated by an equals sign", Name = "--sqlcmdvar", Required = false, Alias = "-sc")]
         public string[] SqlCmdVar { get; set; }
         
-        [CliOption(Description = "Whether to run pre- and postdeployment scripts from references", Name = "--runScriptsFromReferences", Required = false)]
+        [CliOption(Description = "Whether to run pre- and postdeployment scripts from references", Name = "--runScriptsFromReferences", Required = false, Alias = "-sff")]
         public bool RunScriptsFromReferences { get; set; }
         
-        [CliOption(Description = "Encrypt the connection, defaults to false", Required = false, Name = "--encrypt")]
+        [CliOption(Description = "Encrypt the connection, defaults to false", Required = false, Name = "--encrypt", Alias = "-e")]
         public bool Encrypt { get; set; }
         
         public int Run()
