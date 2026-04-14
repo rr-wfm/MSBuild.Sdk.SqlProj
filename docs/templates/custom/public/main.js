192e53b5e5121b4a85a80bfbf2c68686a543f7bc
Neal Mummau <nealmummau@gmail.com>
2026-04-14 08:52:37 -0400

    docs: shift top nav bar to use buttons for GitHub, Nuget, and Releases

---
+++ docs/templates/custom/public/main.js
@@ -0,0 +1,19 @@
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/rr-wfm/MSBuild.Sdk.SqlProj',
+      title: 'GitHub'
+    },
+    {
+      icon: 'box-seam',
+      href: 'https://www.nuget.org/packages/MSBuild.Sdk.SqlProj',
+      title: 'NuGet'
+    },
+    {
+      icon: 'tag',
+      href: 'https://github.com/rr-wfm/MSBuild.Sdk.SqlProj/releases',
+      title: 'Releases'
+    }
+  ]
+}
