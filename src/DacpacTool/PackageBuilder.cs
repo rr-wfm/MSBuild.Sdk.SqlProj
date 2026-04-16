45645d1d03cb278c7207b0918560f63e5cf85d91
Johan van der Slikke <johan.vanderslikke@eggcolumbus.com>
2026-04-16 21:43:23 +0200

    fix

--- src/DacpacTool/PackageBuilder.cs
+++ src/DacpacTool/PackageBuilder.cs
@@ -282,7 +282,7 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
 
             if (_dllReferenced)
             {
-                packageOptions.IgnoreValidationErrors = [ "SQL70557" ];
+                packageOptions.IgnoreValidationErrors = [ "SQL70557", "70557" ];
             }
 
             DacPackageExtensions.BuildPackage(outputFile.FullName, Model, Metadata, packageOptions);
