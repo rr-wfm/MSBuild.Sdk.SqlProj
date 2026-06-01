7b35b2e6b29f5487461dcfeba98ab34d14e853c8
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2026-06-01 12:45:39 +0200

    Remove TSQLSmellSCA analyzer and update docs/rules (#932)
    * Remove TSQLSmellSCA analyzer and update docs/rules
    
    All references to the TSQLSmellSCA analyzer and "Smells" rules have been removed from the codebase, tests, and documentation. Only the ErikEJ.DacFX.SqlServer.Rules package is now recommended and used for SQL code analysis, updated to version 5.0.0. Test logic and assets have been cleaned up accordingly, and documentation reflects these changes.
    
    * fix up
    
    * fix tests

--- src/DacpacTool/PackageAnalyzer.cs
+++ src/DacpacTool/PackageAnalyzer.cs
@@ -47,7 +47,7 @@ namespace MSBuild.Sdk.SqlProj.DacpacTool
 
                 var rules = service.GetRules();
 
-                if (!rules.Any(r => r.Namespace == "SqlServer.Rules" || r.Namespace == "Smells"))
+                if (!rules.Any(r => r.Namespace == "SqlServer.Rules"))
                 {
                     _console.WriteLine("DacpacTool warning SQLPROJ0001: No additional well-known rules files found, consider adding more rules via PackageReference - see the documentation here: https://rr-wfm.github.io/MSBuild.Sdk.SqlProj/docs/static-code-analysis.html. You can ignore this warning by adding '<NoWarn>$(NoWarn);SQLPROJ0001</NoWarn>' to your project file.");
                 }
